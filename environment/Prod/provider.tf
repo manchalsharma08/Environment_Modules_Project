124e97298e44e3a151216b9ee1f791d8aab331fa
manchalsharma08 <159640059+manchalsharma08@users.noreply.github.com>
2025-04-18 09:22:17 +0530

    Update provider.tf
    all done

--- environment/Prod/provider.tf
+++ environment/Prod/provider.tf
@@ -5,6 +5,11 @@ terraform {
       version = "3.104.0"
     }
   }
+backend "azurerm" {
+    resource_group_name  = "my-rg"
+    storage_account_name = "mystorageaccount"
+    container_name       = "tfstate"
+    key                  = "Prod.tfstate"
 }
 
 provider "azurerm" {
